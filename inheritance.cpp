b58342a2d1da2d10aa454eb39f40c42b70b5ab2f
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-03 18:54:02 +0530

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void eat() {
+        cout << "Animal eats" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Dog d;
+    d.eat();  // Inherited method
+    d.bark(); // Method defined in Dog
+    return 0;
+}
