a9b71e62b11bc9b0a3ecbb41251ce7825f9f6ecf
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-04 19:28:44 +0530

    dispaly method

--- eg1.cpp
+++ eg1.cpp
@@ -12,4 +12,8 @@ class Room {
     bool isAvailable;
 
     Room(int number, string type, double price) : roomNumber(number), roomType(type), priceperNight(price), isAvailable(true){};
+
+    void dispalay() const {
+        cout << "Room Number: " << roomNumber << ", Type: " << roomType << ", Price per Night: $" << priceperNight << ", Availability: " << (isAvailable? "Yes" : "No") << endl;
+    }
 }
